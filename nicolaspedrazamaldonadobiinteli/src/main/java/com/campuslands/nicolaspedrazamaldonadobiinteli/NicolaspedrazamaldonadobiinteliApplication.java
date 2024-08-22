1900d02dbd153fbd44302907d936201b4b3bc60c
Nicolas Pedraza Maldonado <152308538+dvn495@users.noreply.github.com>
2024-08-22 08:53:15 -0500

    Add files via upload

---
+++ nicolaspedrazamaldonadobiinteli/src/main/java/com/campuslands/nicolaspedrazamaldonadobiinteli/NicolaspedrazamaldonadobiinteliApplication.java
@@ -0,0 +1,13 @@
+package com.campuslands.nicolaspedrazamaldonadobiinteli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicolaspedrazamaldonadobiinteliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicolaspedrazamaldonadobiinteliApplication.class, args);
+	}
+
+}
---
+++ nicolaspedrazamaldonadobiinteli/src/test/java/com/campuslands/nicolaspedrazamaldonadobiinteli/NicolaspedrazamaldonadobiinteliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campuslands.nicolaspedrazamaldonadobiinteli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicolaspedrazamaldonadobiinteliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
